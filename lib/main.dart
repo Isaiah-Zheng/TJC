9d9ce8b4e1f66f65a7eec313968087fe51d8358f
Isaiah Zheng <zjz88060133@gmail.com>
2025-11-25 10:30:55 +0800

    做的美觀一點

--- lib/main.dart
+++ lib/main.dart
@@ -16,30 +16,82 @@ class MyApp extends StatelessWidget{
         ),
 
         body:Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.person,size:50,color:Colors.blue),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            width: 300,
+                                        //maybe it means that height is infinite
+            decoration: BoxDecoration(
+              color:Colors.blue.shade50,
+              borderRadius: BorderRadius.circular(12),
+            ),
 
-              SizedBox(width:20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
 
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                
-                children: [
-                  Text("Hallelujah",style: TextStyle(color: Colors.black),),
-                  Text("Jesus Love U",style: TextStyle(color: Colors.black),)
-                ],
-              ),
+              children: [
+                Row(
+                  children: [
+                    Icon(Icons.person,size: 50,color:Colors.blue),
+                    SizedBox(width:15),
 
-            ],
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                "Isaiah Zheng",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue,
+                                  letterSpacing: 1
+                                ),
+                              ),
+                              Spacer(
+
+                              ),
+                              Text(
+                                "24/11/25",
+                                style: TextStyle(fontSize: 13,color: Colors.grey),
+                              )
+                            
+
+
+                            ],
+                          
+                          ),
+                          SizedBox(height:5),
+
+                          Text(
+                            "Hallelujah,Jesus loves you forever.",
+                            style: TextStyle(
+                              fontSize: 13
+                            ),
+
+                          )
+                        ],
+                      )
+                      )
+
+                  ]
+                )
+              ],
+
+
+            ),
+          
           ),
 
 
+          
+          
+          
         )
       )
-    );
+    ); 
   } 
 }  
                 
