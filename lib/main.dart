0b680f848bddc6455692f2357c5ca8cc81710eb7
Isaiah Zheng <zjz88060133@gmail.com>
2025-12-08 11:08:22 +0800

    learn how to add memeber and statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -25,103 +25,53 @@ class Member{
   Member({
     required this.name,
     required this.birthday,
-    required this.hope
-  }
-  );
-} 
-
-class HomePage extends StatelessWidget{
-  HomePage({super.key});
-
-  final List<Member> members =  [        //tell me in detail
-    Member(
-      name: "Isaiah Zheng",
-      birthday: "01/01/07",
-      hope: "Hope I can get baptized one day",
-
-    ),
-    Member(
-      name: "Mary Lee",
-      birthday: "05/05/08",
-      hope: "Hope to grow in faith",
-    ),
-    Member(
-      name: "John Tan",
-      birthday: "10/10/06",
-      hope: "Hope to serve the church",
-    ),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:const Text("Members")),
-      body:ListView.builder(
-        itemCount: members.length,
-        itemBuilder: (context,index){
-          final member = members[index];
-          return Card(
-            margin: const EdgeInsets.all(10),
-            child:ListTile(
-              leading:const Icon(Icons.person,size:50,color:Colors.blue),
-              title: Text(member.name),
-              subtitle: Text(member.birthday),
-              onTap:(){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder:(context)=>DetailPage(
-                      name:member.name,
-                      birthday:member.birthday,
-                      hope:member.hope,
-                    )
-                  )
-                );
-              }
-
-            )
-          );
-
-
-        },
-        
-      )
-    );
-
-  }
-}
-
-class DetailPage extends StatelessWidget{
-  final String name;
-  final String birthday;
-  final String hope;
-
-  const DetailPage({
-    super.key,
-    required this.name,
-    required this.birthday,
     required this.hope,
   });
+}
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();  
+}
+
+class _HomePageState extends State<HomePage>{
+  List<Member> members = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title:Text(name)),
-      body:Padding(
-        padding: const EdgeInsets.all(20),
-        child:Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Name:$name",style: const TextStyle(fontSize: 20),),
-            const SizedBox(height: 10,),
-            Text("Birthday:$birthday",style: const TextStyle(fontSize: 18),),
-            SizedBox(height: 10,),
-            Text("Hope:$hope",style: TextStyle(fontSize: 18),)
-          ],
-        )
-      )
-    );
     
-  }
+    return Scaffold(
+      appBar: AppBar(title: Text("Members"),),
+      body:ListView.builder(
+        itemCount:members.length,
+        itemBuilder:(context,index){
+          final member = members[index];
+          return ListTile(
+            title: Text(member.name),
+          );
+          
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:(){
+          setState(() {
+            members.add(Member(
+              name:"New Member",
+              birthday:"01/01/01",
+              hope:"New hope message",
+            ));
 
+            
+          });
+
+        },
+        child:Icon(Icons.add)
+
+      ),
+    );
+  
+  }
+  
 }
